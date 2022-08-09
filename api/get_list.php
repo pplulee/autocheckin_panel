6f7ab3a0ef5b95b4da5b1cd8d899a3dafe74a8f9
022by <46434255+pplulee@users.noreply.github.com>
2022-08-09 17:47:25 +0100

    更新get_list

--- api/get_list.php
+++ api/get_list.php
@@ -1,1 +1,19 @@
 <?php
+include("header.php");
+$result = mysqli_query($conn, "SELECT id FROM tasks;");
+if (mysqli_num_rows($result) == 0) {
+    $data = array(
+        'status' => 'fail',
+        'message' => '没有任务'
+    );
+}else{
+    $list = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $list[] = $row['id'];
+    }
+    $data = array(
+        'status' => 'success',
+        'id_list' => implode(",",$list)
+    );
+}
+echo json_encode($data,JSON_UNESCAPED_UNICODE);
