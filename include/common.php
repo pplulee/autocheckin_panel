116c3008156ba01e12fddcd016f7577d5854474d
022by <46434255+pplulee@users.noreply.github.com>
2022-08-11 14:40:42 +0100

    修复折叠菜单无法展开

--- include/common.php
+++ include/common.php
@@ -33,5 +33,8 @@ echo '<!DOCTYPE html>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.2/font/bootstrap-icons.css">
     <link href=”https://fonts.googleapis.com/icon?family=Material+Icons” rel=”stylesheet”>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+        crossorigin="anonymous"></script>
 </head>
 ';
