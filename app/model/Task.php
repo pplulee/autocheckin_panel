cdad2a98ffdbe48ad854a9e513a92aec44adbbe4
SenlIn <xinyu.li-20@student.manchester.ac.uk>
2023-03-15 16:32:09 +0000

    添加Task基础模型

---
+++ app/model/Task.php
@@ -0,0 +1,81 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\model;
+
+use think\Model;
+
+/**
+ * @mixin \think\Model
+ */
+class Task extends Model
+{
+    protected $table = 'tasks';
+    protected $pk = 'id';
+
+    public function fetch($id)
+    {
+        $task = $this->where('id', $id)->find();
+        if ($task) {
+            return $task;
+        }
+        return null;
+    }
+
+    public function addTask($data): bool
+    {
+        $task = new Task();
+        $username = $data['username'];
+        $password = $data['password'];
+        $tgbot_userid = $data['tgbot_chat_id'];
+        $tgbot_token = $data['tgbot_token'];
+        $wxpusher_uid = $data['wxpusher_uid'];
+        $webdriver_url = $data['webdriver_url'];
+        $userid = $data['userid'];
+        $task->save(['username' => $username,
+                     'password' => $password,
+                     'tgbot_chat_id' => $tgbot_userid,
+                     'tgbot_token' => $tgbot_token,
+                     'wxpusher_uid' => $wxpusher_uid,
+                     'webdriver_url' => $webdriver_url,
+                     'userid' => $userid]);
+        return true;
+    }
+
+    public function updateTask($id,$data): bool
+    {
+        // check task exist
+        $task = $this->fetch($id);
+        if (!$task) {
+            return false;
+        } else {
+            $username = $data['username'];
+            $password = $data['password'];
+            $tgbot_userid = $data['tgbot_chat_id'];
+            $tgbot_token = $data['tgbot_token'];
+            $wxpusher_uid = $data['wxpusher_uid'];
+            $webdriver_url = $data['webdriver_url'];
+            $userid = $data['userid'];
+            $task->update(['username' => $username,
+                           'password' => $password,
+                           'tgbot_chat_id' => $tgbot_userid,
+                           'tgbot_token' => $tgbot_token,
+                           'wxpusher_uid' => $wxpusher_uid,
+                           'webdriver_url' => $webdriver_url,
+                           'userid' => $userid]);
+            return true;
+        }
+    }
+
+    public function deleteTask($id): bool
+    {
+        // check task exist
+        $task = $this->fetch($id);
+        if (!$task) {
+            return false;
+        } else {
+            $task->delete();
+            return true;
+        }
+    }
+}
