e9f6c7e78202eeea0f35aa00461ae826681acb8c
022by <46434255+pplulee@users.noreply.github.com>
2023-03-15 00:02:16 +0000

    增加基于sweetalert2的弹窗功能

--- app/common.php
+++ app/common.php
@@ -1,2 +1,39 @@
 <?php
 // 应用公共文件
+function alert($type, $message, $delay, $dest)
+{
+    echo '
+    <script src="/static/js/sweetalert2.all.min.js"></script>
+    <link href="/static/css/sweetalert2.min.css" rel="stylesheet">
+    ';
+    switch ($type) {
+        case "success":
+            $title = "成功";
+            break;
+        case "error":
+            $title = "错误";
+            break;
+        case "warning":
+            $title = "警告";
+            break;
+        case "info":
+            $title = "信息";
+            break;
+        case "question":
+            $title = "请检查";
+            break;
+        default:
+            $title = "";
+            break;
+    }
+    return "<script>window.onload = function() {
+            Swal.fire({
+            icon: '$type',
+            title: '$title',
+            text: '$message',
+            timer:$delay,
+            showConfirmButton: false,
+            timerProgressBar: true});
+            setTimeout(\"javascript:location.href='$dest'\", $delay);}
+            </script>";
+}
--- app/controller/UserController.php
+++ app/controller/UserController.php
@@ -12,38 +12,37 @@ class UserController extends BaseController
     public function index()
     {
         if (!Session::get('user_id')) {
-            return redirect('/user/login');
+            return redirect('/index');
         }
         $user = new User();
         $user = $user->fetch(Session::get('user_id'));
-        return view('user/index', ['user' => $user]);
+        return view('/user/index', ['user' => $user]);
     }
 
     public function login()
     {
         $email = $this->request->post('email');
         $password = $this->request->post('password');
+        if ($email == null || $password == null) {
+            return alert("error", "用户名或密码不能为空", "2000", "/index");
+        }
         $user = new User();
         if ($this->request->post('login')) {
             $user = $user->login($email, $password);
             if ($user) {
-                echo "登录成功";
-                return redirect('/user/index');
+                return alert("success", "登录成功", "2000", "/user/index");
             } else {
-                echo "登录失败";
-                return redirect('/index');
+                return alert("error", "用户名或密码错误", "2000", "/index");
             }
         } else if ($this->request->post('register')) {
             $user = $user->register($email, $password);
             if ($user) {
-                echo "注册成功";
-                return redirect('/index');
+                return alert("success", "注册成功", "2000", "/index");
             } else {
-                echo "注册失败";
-                return redirect('/index');
+                return alert("error", "用户已存在", "2000", "/index");
             }
         } else {
-            return view('user/login');
+            return alert("error", "未知错误", "2000", "/index");
         }
     }
 
@@ -51,20 +50,20 @@ class UserController extends BaseController
     public function logout()
     {
         Session::delete('user_id');
-        echo "退出成功";
-        return redirect('/index');
+        return alert("success", "登出成功", "2000", "/index");
     }
 
     public function update()
     {
         $email = $this->request->post('email');
-        if ($email == null) {
-            return redirect('/user/index');
-        }
+        // 需要改进验证方法
+//        if ($email == null) {
+//            return redirect('/user/index');
+//        }
         $password = $this->request->post('password');
         $user = new User();
         $user = $user->fetch(Session::get('user_id'));
         $user->updateUser(Session::get('user_id'), ['email' => $email, 'password' => $password]);
-        return redirect('/user/index');
+        return alert("success", "修改成功", "2000", "/user/index");
     }
 }
